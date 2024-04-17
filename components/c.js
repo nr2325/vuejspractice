2a0e2ce8bc2d96e89d32ea7b8f5284e08ca1845f
nr232 <naziurrahman232@gmail.com>
2024-04-17 22:57:10 +0600

    simple component

---
+++ components/c.js
@@ -0,0 +1,15 @@
+app.component("sample-component", {    
+    template:
+    
+    `
+    <div>
+    <p>This is a component</p>
+    <ol>
+    <li>Green</li>
+    <li>Red</li>
+    <li>Blue</li>
+    
+    </ol>
+    </div>
+    `
+});
